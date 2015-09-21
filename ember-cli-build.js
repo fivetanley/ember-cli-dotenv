0ea4799eb9f3aeefb46c1b467b8a93be59053198
Jordan Hawker <hawker.jordan@gmail.com>
2015-09-21 14:26:14 -0700

    Upgrade Addon via `ember init` command

---
+++ ember-cli-build.js
@@ -0,0 +1,23 @@
+/* global require, module */
+var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+
+module.exports = function(defaults) {
+  var app = new EmberAddon(defaults, {
+    dotEnv: {
+      clientAllowedKeys: ['DOTENV_VAR']//,
+      // path: './.env.custom',
+      // path: {
+      //   test: './.env.custom'
+      // }
+    }
+  });
+
+  /*
+    This build file specifes the options for the dummy test app of this
+    addon, located in `/tests/dummy`
+    This build file does *not* influence how the addon or the app using it
+    behave. You most likely want to be modifying `./index.js` or app's build file
+  */
+
+  return app.toTree();
+};
--- tests/integration/smoke-test.js
+++ tests/integration/smoke-test.js
@@ -1,29 +1,31 @@
+import { module } from 'qunit';
+import { test } from 'ember-qunit';
 import ENV from "dummy/config/environment";
 
 module("smoke test");
 
 // comment out below if dotEnv.path is specified in Procfile
-test("should work", function(){
-  equal(ENV.DOTENV_VAR, "dotenv");
+test("should work", function(assert){
+  assert.equal(ENV.DOTENV_VAR, "dotenv");
 });
 
-test("doesn't put in keys unless they are explicitly allowed", function(){
-  equal(Object.hasOwnProperty.call(ENV, "DO_NOT_ALLOW"), false);
+test("doesn't put in keys unless they are explicitly allowed", function(assert){
+  assert.equal(Object.hasOwnProperty.call(ENV, "DO_NOT_ALLOW"), false);
 });
 
-test("puts keys in the node process ENV always", function(){
-  equal(ENV.IN_PROCESS_ENV, "IN_PROCESS_ENV");
+test("puts keys in the node process ENV always", function(assert){
+  assert.equal(ENV.IN_PROCESS_ENV, "IN_PROCESS_ENV");
 });
 
 // uncomment out below if dotEnv.path is specified in Procfile
-// test("should work", function(){
-//   equal(ENV.DOTENV_VAR, "custom.dotenv");
+// test("should work", function(assert){
+//   assert.equal(ENV.DOTENV_VAR, "custom.dotenv");
 // });
 
-// test("doesn't put in keys unless they are explicitly allowed", function(){
-//   equal(Object.hasOwnProperty.call(ENV, 'DO_NOT_ALLOW'), false);
+// test("doesn't put in keys unless they are explicitly allowed", function(assert){
+//   assert.equal(Object.hasOwnProperty.call(ENV, 'DO_NOT_ALLOW'), false);
 // });
 
-// test("puts keys in the node process ENV always", function(){
-//   equal(ENV.IN_PROCESS_ENV, "CUSTOM_IN_PROCESS_ENV");
+// test("puts keys in the node process ENV always", function(assert){
+//   assert.equal(ENV.IN_PROCESS_ENV, "CUSTOM_IN_PROCESS_ENV");
 // });
